2d191deec91bbef75fa72c62d7f7ef3c21105cd8
taras-d <taras.datsenko@gmail.com>
2019-05-21 10:20:24 +0300

    Create temp window to remove script

--- options/options.js
+++ options/options.js
@@ -1,20 +1,38 @@
-const tableBody = $('.table tbody');
+import {
+  getFromLocalStorage,
+  setToLocalStorage,
+  createWindow,
+  removeWindow,
+  executeScript
+} from '../util.js';
 
+let $tBody;
 let scripts;
 
-function scriptsLoaded(res) {
-  scripts = res || [];
-  renderScripts();
+function init() {
+  $tBody = $('.table tbody');
+
+  getFromLocalStorage(['scripts']).then(data => {
+    scripts = data.scripts;
+    renderScripts();
+  });
+
+  chrome.storage.local.onChanged.addListener(data => {
+    if ('scripts' in data) {
+      scripts = data.scripts.newValue;
+      renderScripts();
+    }
+  });
 }
 
 function renderScripts() {
-  tableBody.empty();
-  if (scripts.length) {
+  $tBody.empty();
+  if (scripts && scripts.length) {
     scripts.forEach(script => {
-      tableBody.append(createTableRow(script));
+      $tBody.append(createTableRow(script));
     })
   } else {
-    tableBody.append(
+    $tBody.append(
       $('<tr>').append(
         $('<td>', {
           text: 'no data',
@@ -24,20 +42,6 @@ function renderScripts() {
       )
     );
   }
-}
-
-function formatDate(date) {
-  date = new Date(date);
-  return [
-    date.getDate(), '.', date.getMonth() + 1, '.', date.getFullYear(), ' ',
-    date.getHours(), ':', date.getMinutes(), ':', date.getMilliseconds()
-  ].map(part => {
-    if (typeof part === 'number') {
-      return part < 10 ? `0${part}` : `${part}`;
-    } else {
-      return part;
-    }
-  }).join('');
 }
 
 function createTableRow(script, index) {
@@ -82,20 +86,39 @@ function createTableRow(script, index) {
   );
 }
 
+function formatDate(date) {
+  date = new Date(date);
+  return [
+    date.getDate(), '.', date.getMonth() + 1, '.', date.getFullYear(), ' ',
+    date.getHours(), ':', date.getMinutes()
+  ].map(part => {
+    if (typeof part === 'number') {
+      return part < 10 ? `0${part}` : `${part}`;
+    } else {
+      return part;
+    }
+  }).join('');
+}
+
 function editClick(script, index) {
   console.log(script);
 }
 
 function deleteClick(script, index) {
-  if (confirm('Are you sure?')) {
-    scripts.splice(index, 1);
-    chrome.storage.local.set({ scripts });
+  if (!confirm(`Delete script for "${script.origin}"?`)) {
+    return;
   }
+
+  let wnd;
+  createWindow({ url: script.origin, state: 'minimized' }).then(res => {
+    wnd = res;
+    return executeScript(wnd.tabs[0].id, { code: 'delete localStorage["RunJsOnPageStart"]' });
+  }).then(() => {
+    return removeWindow(wnd.id);
+  }).then(() => {
+    scripts.splice(index, 1);
+    return setToLocalStorage({ scripts });
+  });
 }
 
-chrome.storage.local.get(['scripts'], data => scriptsLoaded(data.scripts));
-chrome.storage.local.onChanged.addListener(data => {
-  if ('scripts' in data) {
-    scriptsLoaded(data.scripts.newValue)
-  }
-});
+init();
--- util.js
+++ util.js
@@ -13,3 +13,5 @@ export const getFromLocalStorage = toPromise(chrome.storage.local, 'get');
 export const getSelectedTab = toPromise(chrome.tabs, 'getSelected');
 export const executeScript = toPromise(chrome.tabs, 'executeScript');
 export const reloadTab = toPromise(chrome.tabs, 'reload');
+export const createWindow = toPromise(chrome.windows, 'create');
+export const removeWindow = toPromise(chrome.windows, 'remove');
