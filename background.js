5cf27f6884d818bebfa9e581eca9f9806610193a
taras-d <taras.datsenko@gmail.com>
2019-05-27 08:27:58 +0300

    Remove unused ace modules

--- background.js
+++ background.js
@@ -1,8 +1,4 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (!changeInfo.url) {
-    return;
-  }
-
   if (/^(file|http|https):\/\//i.test(changeInfo.url)) {
     chrome.browserAction.enable(tabId);
   } else {
