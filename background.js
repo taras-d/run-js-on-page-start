43c49b0017e118df3286357d1de614c20a865827
taras-d <taras.datsenko@gmail.com>
2019-06-25 10:42:14 +0300

    Update background script

--- background.js
+++ background.js
@@ -1,11 +1,15 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (!changeInfo.url) {
-    return;
-  }
-
-  if (/^(ftp|file|http|https):\/\//i.test(changeInfo.url)) {
+function toggleBrowserAction(tabId, url) {
+  if (/^(ftp|file|http|https):\/\//i.test(url)) {
     chrome.browserAction.enable(tabId);
   } else {
     chrome.browserAction.disable(tabId);
+  }
+}
+
+chrome.tabs.getSelected(tab => toggleBrowserAction(tab.id, tab.url));
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  if (changeInfo.url) {
+    toggleBrowserAction(tabId, changeInfo.url);
   }
 });
